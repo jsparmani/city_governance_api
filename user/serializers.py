85f97a0ccf6f7c445608a3c51faf298a616a7dcd
Jay Parmani <jsparmani@gmail.com>
2019-10-17 12:31:09 +0530

    user authentication api

---
+++ user/serializers.py
@@ -0,0 +1,57 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model, authenticate
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+
+
+class AuthTokenSerializer(serializers.Serializer):
+    """Serializer for the user authentication object"""
+
+    username = serializers.CharField()
+    password = serializers.CharField(
+        style={'input_type': 'password'},
+        trim_whitespace=False
+    )
+
+    def validate(self, attrs):
+        """Validate and authenticate the user"""
+
+        username = attrs.get('username'),
+        password = attrs.get('password')
+        user = authenticate(
+            request=self.context.get('request'),
+            username=username[0],
+            password=password
+        )
+
+        if not user:
+            msg = 'Unable to authenticate with provided credentials'
+
+            raise serializers.ValidationError(msg, code='authentication')
+
+        attrs['user'] = user
+        return attrs
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """Serializer for the users object"""
+
+    password = serializers.CharField(
+        style={'input_type': 'password'},
+        trim_whitespace=False
+    )
+
+    class Meta:
+        model = get_user_model()
+        fields = ('id', 'username', 'password')
+        read_only_fields = ('id', )
+        extra_kwargs = {
+            'password': {'write_only': True, 'min_length': 5}
+        }
+
+    def create(self, validated_data):
+        user = get_user_model().objects.create(
+            username=validated_data['username'])
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
---
+++ user/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from . import views
+from rest_framework.routers import DefaultRouter
+
+
+app_name = 'user'
+
+urlpatterns = [
+    path('create/', views.CreateUserView.as_view(), name='create'),
+    path('token/', views.CreateTokenView.as_view(), name='token'),
+]
